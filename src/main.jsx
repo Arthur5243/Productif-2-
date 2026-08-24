cdb86dcd501db7d585a3a2f4b674b638a2580e1c
Arthur5243 <arthur.cambin@gmail.com>
2026-08-24 09:18:14 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PlannerApp from './PlannerApp.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PlannerApp />
+  </React.StrictMode>
+);
